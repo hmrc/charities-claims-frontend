3c1309e83508aa4712b9c010e3cb881b06bd06ab
westwater <gmwestwater@hotmail.co.uk>
2025-11-24 16:05:02 +0000

    DTR-1363 Added character count viewmodel

---
+++ app/viewmodels/govuk/CharacterCountFluency.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.govuk
+
+import play.api.data.Field
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.charactercount.CharacterCount
+import uk.gov.hmrc.govukfrontend.views.viewmodels.hint.Hint
+import uk.gov.hmrc.govukfrontend.views.viewmodels.label.Label
+import viewmodels.ErrorMessageAwareness
+
+object characterCount extends CharacterCountFluency
+
+trait CharacterCountFluency {
+
+  object CharacterCountViewModel extends ErrorMessageAwareness {
+
+    def apply(
+      field: Field,
+      label: Label
+    )(implicit messages: Messages): CharacterCount =
+      CharacterCount(
+        id = field.id,
+        name = field.name,
+        value = field.value,
+        label = label,
+        errorMessage = errorMessage(field),
+        rows = 1
+      )
+  }
+
+  implicit class FluentCharacterCount(characterCount: CharacterCount) {
+
+    def withId(id: String): CharacterCount =
+      characterCount.copy(id = id)
+
+    def withHint(hint: Hint): CharacterCount =
+      characterCount.copy(hint = Some(hint))
+
+    def withCssClass(newClass: String): CharacterCount =
+      characterCount.copy(classes = s"${characterCount.classes} $newClass")
+
+    def noResize: CharacterCount =
+      characterCount.withCssClass("no-resize")
+
+    def withAttribute(attribute: (String, String)): CharacterCount =
+      characterCount.copy(attributes = characterCount.attributes + attribute)
+
+    def withSpellcheck(on: Boolean = true): CharacterCount =
+      characterCount.copy(spellcheck = Some(on))
+  }
+}
