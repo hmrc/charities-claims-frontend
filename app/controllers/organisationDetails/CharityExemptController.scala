818ad5ecbd64b0ff636c46c563170a34dcb65bcf
nimo-mohamed <138133949+nimo-mohamed@users.noreply.github.com>
2025-12-09 15:00:21 +0000

    DTR-1411: CHARITIES: Screen A2.4- Your charity is exempt

---
+++ app/controllers/organisationDetails/CharityExemptController.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.organisationDetails
+
+import com.google.inject.Inject
+import play.api.i18n.I18nSupport
+import controllers.actions.Actions
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.CharityExemptView
+
+import scala.concurrent.Future
+
+class CharityExemptController @Inject() (
+  val controllerComponents: MessagesControllerComponents,
+  actions: Actions,
+  view: CharityExemptView
+) extends FrontendBaseController
+    with I18nSupport {
+
+  val onPageLoad: Action[AnyContent] = actions.authAndGetData() { implicit request =>
+    Ok(view())
+  }
+
+  val onSubmit: Action[AnyContent] = actions.authAndGetData().async { implicit request =>
+    // TODO: When ticket A2.6 (Corporate Trustee) is created, change this to:
+    Future.successful(Redirect(routes.CharityExemptController.onPageLoad))
+  }
+}
