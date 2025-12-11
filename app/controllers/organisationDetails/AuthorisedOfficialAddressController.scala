1caa95af53d74d0d51323db873c296eb782692a0
Purbai <243989389+pvarsani@users.noreply.github.com>
2025-12-11 15:24:00 +0000

    DTR 2001 - screen A2.8 (#84)
    * screen A2.8
    
    * corrected typo

---
+++ app/controllers/organisationDetails/AuthorisedOfficialAddressController.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.organisationDetails
+
+import com.google.inject.Inject
+import controllers.BaseController
+import controllers.actions.Actions
+import forms.YesNoFormProvider
+import models.OrganisationDetailsAnswers
+import models.Mode
+import models.Mode.*
+import play.api.data.Form
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import services.SaveService
+import views.html.AuthorisedOfficialAddressView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class AuthorisedOfficialAddressController @Inject() (
+  val controllerComponents: MessagesControllerComponents,
+  view: AuthorisedOfficialAddressView,
+  actions: Actions,
+  formProvider: YesNoFormProvider,
+  saveService: SaveService
+)(using ec: ExecutionContext)
+    extends BaseController {
+
+  val form: Form[Boolean] = formProvider("authorisedOfficialAddress.error.required")
+
+  def onPageLoad(mode: Mode = NormalMode): Action[AnyContent] = actions.authAndGetData().async { implicit request =>
+    if OrganisationDetailsAnswers.getAreYouACorporateTrustee.contains(false)
+    then {
+      val previousAnswer = OrganisationDetailsAnswers.getDoYouHaveUKAddress
+      Future.successful(Ok(view(form.withDefault(previousAnswer), mode)))
+    } else { Future.successful(Redirect(controllers.routes.PageNotFoundController.onPageLoad)) }
+  }
+
+  def onSubmit(mode: Mode = NormalMode): Action[AnyContent] = actions.authAndGetData().async { implicit request =>
+    form
+      .bindFromRequest()
+      .fold(
+        formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode))),
+        value =>
+          saveService
+            .save(OrganisationDetailsAnswers.setDoYouHaveUKAddress(value))
+            .map { _ =>
+              Redirect(routes.AuthorisedOfficialAddressController.onPageLoad(NormalMode))
+            }
+      )
+  }
+}
---
+++ test/controllers/organisationDetails/AuthorisedOfficialAddressControllerSpec.scala
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.organisationDetails
+
+import play.api.test.FakeRequest
+import play.api.mvc.{AnyContentAsEmpty, AnyContentAsFormUrlEncoded}
+import controllers.ControllerSpec
+import views.html.AuthorisedOfficialAddressView
+import play.api.Application
+import forms.YesNoFormProvider
+import models.{OrganisationDetailsAnswers, SessionData}
+import play.api.data.Form
+import models.Mode.*
+
+class AuthorisedOfficialAddressControllerSpec extends ControllerSpec {
+  private val form: Form[Boolean] = new YesNoFormProvider()()
+  "AuthorisedOfficialAddressController" - {
+    "onPageLoad" - {
+      "should render the page correctly if Corporate trustee is false" in {
+        val sessionData                = OrganisationDetailsAnswers.setAreYouACorporateTrustee(false)
+        given application: Application = applicationBuilder(sessionData = sessionData).build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(GET, routes.AuthorisedOfficialAddressController.onPageLoad(NormalMode).url)
+          val result                                         = route(application, request).value
+          val view                                           = application.injector.instanceOf[AuthorisedOfficialAddressView]
+
+          status(result) shouldEqual OK
+          contentAsString(result) shouldEqual view(form, NormalMode).body
+        }
+      }
+
+      "should render page not found if Corporate trustee is true" in {
+        val sessionData = OrganisationDetailsAnswers.setAreYouACorporateTrustee(true)
+
+        given application: Application = applicationBuilder(sessionData = sessionData).build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(GET, routes.AuthorisedOfficialAddressController.onPageLoad(NormalMode).url)
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(controllers.routes.PageNotFoundController.onPageLoad.url)
+        }
+      }
+    }
+
+    "onSubmit" - {
+      "should redirect to the next page when the value is true" in {
+        given application: Application = applicationBuilder().mockSaveSession.build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsFormUrlEncoded] =
+            FakeRequest(POST, routes.AuthorisedOfficialAddressController.onSubmit(NormalMode).url)
+              .withFormUrlEncodedBody("value" -> "true")
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(
+            routes.AuthorisedOfficialAddressController.onPageLoad(NormalMode).url
+          )
+        }
+      }
+
+      "should redirect to the next page when the value is false" in {
+        given application: Application = applicationBuilder().mockSaveSession.build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsFormUrlEncoded] =
+            FakeRequest(POST, routes.AuthorisedOfficialAddressController.onSubmit(NormalMode).url)
+              .withFormUrlEncodedBody("value" -> "false")
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(
+            routes.AuthorisedOfficialAddressController.onPageLoad(NormalMode).url
+          )
+        }
+      }
+
+      "should reload the page with errors when a required field is missing" in {
+        given application: Application = applicationBuilder().build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsFormUrlEncoded] =
+            FakeRequest(POST, routes.AuthorisedOfficialAddressController.onSubmit(NormalMode).url)
+              .withFormUrlEncodedBody("other" -> "field")
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual BAD_REQUEST
+        }
+      }
+    }
+  }
+}
