0c0ee38b2aa34ddb95e0b31ad61d2b9f351f63a0
nimo-mohamed <138133949+nimo-mohamed@users.noreply.github.com>
2025-12-17 16:15:56 +0000

    DTR-2003: CHARITIES: Screen A2.10- Authorised official details
    # Conflicts:
    #	conf/app.routes
    
    # Conflicts:
    #	conf/app.routes

---
+++ app/controllers/organisationDetails/AuthorisedOfficialDetailsController.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.organisationDetails
+
+import com.google.inject.Inject
+import controllers.actions.Actions
+import forms.AuthorisedOfficialDetailsFormProvider
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.AuthorisedOfficialDetailsView
+
+import scala.concurrent.Future
+
+class AuthorisedOfficialDetailsController @Inject() (
+  val controllerComponents: MessagesControllerComponents,
+  actions: Actions,
+  formProvider: AuthorisedOfficialDetailsFormProvider,
+  view: AuthorisedOfficialDetailsView
+) extends FrontendBaseController
+    with I18nSupport {
+
+  val form = formProvider()
+
+  def onPageLoad: Action[AnyContent] = actions.authAndGetData() { implicit request =>
+    Ok(view(form))
+  }
+
+  def onSubmit: Action[AnyContent] = actions.authAndGetData().async { implicit request =>
+    form
+      .bindFromRequest()
+      .fold(
+        formWithErrors => Future.successful(BadRequest(view(formWithErrors))),
+        value =>
+          // TODO: Redirect to A2.11 - Check your answers
+          Future.successful(Redirect(routes.AuthorisedOfficialDetailsController.onPageLoad))
+      )
+  }
+}
---
+++ app/forms/AuthorisedOfficialDetailsFormProvider.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import javax.inject.Inject
+import models.AuthorisedOfficialDetails
+import play.api.data.Form
+import play.api.data.Forms._
+
+class AuthorisedOfficialDetailsFormProvider @Inject() {
+
+  private val titleRegex     = "^([a-zA-Z]{1,4})$"
+  private val firstNameRegex = "^([a-zA-Z][a-zA-Z\\- ]*)$"
+  private val lastNameRegex  = "^[A-Za-z0-9 ,\\.\\(\\)/&\\!\\-']+$"
+  private val phoneRegex     = "^[0-9\\(\\)\\/\\-\\s]{1,35}$"
+  private val postcodeRegex  =
+    "^\\s*((GIR 0AA)|((([a-zA-Z][0-9][0-9]?)|(([a-zA-Z][a-zA-Z][0-9][0-9]?)|(([a-zA-Z][0-9][a-zA-Z])|([a-zA-Z][a-zA-Z][0-9][a-zA-Z]))))\\s?[0-9][a-zA-Z]{2}))\\s*$"
+
+  def apply(): Form[AuthorisedOfficialDetails] = Form(
+    mapping(
+      "title"       -> optional(
+        text
+          .verifying("authorisedOfficialDetails.title.error.format", _.matches(titleRegex))
+          .verifying("authorisedOfficialDetails.title.error.length", _.length <= 4)
+      ),
+      "firstName"   -> text
+        .verifying("authorisedOfficialDetails.firstName.error.required", _.nonEmpty)
+        .verifying(
+          "authorisedOfficialDetails.firstName.error.format",
+          name => name.isEmpty || name.matches(firstNameRegex)
+        )
+        .verifying("authorisedOfficialDetails.firstName.error.length", _.length <= 35),
+      "lastName"    -> text
+        .verifying("authorisedOfficialDetails.lastName.error.required", _.nonEmpty)
+        .verifying(
+          "authorisedOfficialDetails.lastName.error.format",
+          name => name.isEmpty || name.matches(lastNameRegex)
+        )
+        .verifying("authorisedOfficialDetails.lastName.error.length", _.length <= 35),
+      "phoneNumber" -> text
+        .verifying("authorisedOfficialDetails.phoneNumber.error.required", _.nonEmpty)
+        .verifying(
+          "authorisedOfficialDetails.phoneNumber.error.format",
+          phone => phone.isEmpty || phone.matches(phoneRegex)
+        )
+        .verifying("authorisedOfficialDetails.phoneNumber.error.length", _.length <= 35),
+      "postcode"    -> optional(
+        text
+          .verifying("authorisedOfficialDetails.postcode.error.format", _.toUpperCase.matches(postcodeRegex))
+          .verifying("authorisedOfficialDetails.postcode.error.length", _.length <= 8)
+      )
+    )(AuthorisedOfficialDetails.apply)(o => Some((o.title, o.firstName, o.lastName, o.phoneNumber, o.postcode)))
+  )
+}
