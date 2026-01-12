32a5aea2a26518ef59159c6998c0c5704d8df851
Simon Hayden <244017492+simonhdw@users.noreply.github.com>
2026-01-12 14:17:34 +0000

    [DTR-1859] WRN1.0 connected to stub for getting schedule summary and deletion, added placeholder screens for testing (#124)
    Set up WRN1.0 delete gift aid schedule data screen.
    Connect to stub for getting summary of schedule upload and add delete.
    Redirects added to placeholder screens for testing.

---
+++ test/models/UploadSummarySpec.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import util.BaseSpec
+import play.api.libs.json.Json
+
+class UploadSummarySpec extends BaseSpec {
+
+  "UploadSummary" - {
+    "be serialised and deserialised correctly with uploadUrl" in {
+      val uploadSummary = UploadSummary(
+        reference = "test-ref-123",
+        validationType = "GiftAid",
+        fileStatus = "VALIDATED",
+        uploadUrl = Some("https://example.com/upload")
+      )
+      Json.parse(Json.prettyPrint(Json.toJson(uploadSummary))).as[UploadSummary] shouldBe uploadSummary
+    }
+
+    "be serialised and deserialised correctly without uploadUrl" in {
+      val uploadSummary = UploadSummary(
+        reference = "test-ref-456",
+        validationType = "OtherIncome",
+        fileStatus = "VALIDATING",
+        uploadUrl = None
+      )
+      Json.parse(Json.prettyPrint(Json.toJson(uploadSummary))).as[UploadSummary] shouldBe uploadSummary
+    }
+  }
+}
