2690ead10b560732af736516a12f215f3dabbdca
pvarsani <243989389+pvarsani@users.noreply.github.com>
2026-01-21 12:44:50 +0000

    new screen R1.3-claim comm building

---
+++ test/controllers/repaymentclaimdetails/ClaimingCommunityBuildingDonationsControllerSpec.scala
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.repaymentclaimdetails
+
+import play.api.test.FakeRequest
+import play.api.mvc.{AnyContentAsEmpty, AnyContentAsFormUrlEncoded}
+import controllers.ControllerSpec
+import play.api.Application
+import forms.YesNoFormProvider
+import play.api.data.Form
+import models.RepaymentClaimDetailsAnswers
+import models.Mode.*
+import views.html.ClaimingCommunityBuildingDonationsView
+
+class ClaimingCommunityBuildingDonationsControllerSpec extends ControllerSpec {
+  private val form: Form[Boolean] = new YesNoFormProvider()()
+
+  "ClaimingCommunityBuildingDonationsController" - {
+    "onPageLoad" - {
+      "should render the page correctly" in {
+        given application: Application = applicationBuilder().build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(GET, routes.ClaimingCommunityBuildingDonationsController.onPageLoad(NormalMode).url)
+
+          val result = route(application, request).value
+          val view   = application.injector.instanceOf[ClaimingCommunityBuildingDonationsView]
+
+          status(result) shouldEqual OK
+          contentAsString(result) shouldEqual view(form, NormalMode).body
+        }
+      }
+
+      "should render the page and pre-populate correctly with true value" in {
+
+        val sessionData = RepaymentClaimDetailsAnswers.setClaimingDonationsCollectedInCommunityBuildings(true)
+
+        given application: Application = applicationBuilder(sessionData = sessionData).build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(GET, routes.ClaimingCommunityBuildingDonationsController.onPageLoad(NormalMode).url)
+
+          val result = route(application, request).value
+          val view   = application.injector.instanceOf[ClaimingCommunityBuildingDonationsView]
+
+          status(result) shouldEqual OK
+          contentAsString(result) shouldEqual view(form.fill(true), NormalMode).body
+        }
+      }
+
+      "should render the page and pre-populate correctly with false value" in {
+
+        val sessionData = RepaymentClaimDetailsAnswers.setClaimingDonationsCollectedInCommunityBuildings(false)
+
+        given application: Application = applicationBuilder(sessionData = sessionData).build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(GET, routes.ClaimingCommunityBuildingDonationsController.onPageLoad(NormalMode).url)
+
+          val result = route(application, request).value
+          val view   = application.injector.instanceOf[ClaimingCommunityBuildingDonationsView]
+
+          status(result) shouldEqual OK
+          contentAsString(result) shouldEqual view(form.fill(false), NormalMode).body
+        }
+      }
+    }
+
+    "onSubmit" - {
+      "should redirect to the next page when the value is true" in {
+
+        given application: Application = applicationBuilder().mockSaveSession.build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsFormUrlEncoded] =
+            FakeRequest(POST, routes.ClaimingCommunityBuildingDonationsController.onSubmit(NormalMode).url)
+              .withFormUrlEncodedBody("value" -> "true")
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(
+            routes.ClaimingCommunityBuildingDonationsController.onPageLoad(NormalMode).url
+          )
+        }
+      }
+
+      "should redirect to the next page when the value is false" in {
+        given application: Application = applicationBuilder().mockSaveSession.build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsFormUrlEncoded] =
+            FakeRequest(POST, routes.ClaimingCommunityBuildingDonationsController.onSubmit(NormalMode).url)
+              .withFormUrlEncodedBody(
+                "value" -> "false"
+              )
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(
+            routes.ClaimingCommunityBuildingDonationsController.onPageLoad(NormalMode).url
+          )
+        }
+      }
+
+    }
+
+  }
+}
