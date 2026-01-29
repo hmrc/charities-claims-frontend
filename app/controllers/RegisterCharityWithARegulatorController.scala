8d804bd34f239bf929ca480cd2ca04538e0a29d2
Simon Hayden <244017492+simonhdw@users.noreply.github.com>
2026-01-29 12:23:34 +0000

    [DTR-2185] WRN5 screen confirmation if need to register charity with regulator (#163)
    Added D3 screen placeholder, add dynamic values for limits in view, update tests.
    F9 integration still to be implemented.

---
+++ app/controllers/RegisterCharityWithARegulatorController.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import com.google.inject.Inject
+import config.FrontendAppConfig
+import controllers.BaseController
+import controllers.actions.Actions
+import forms.YesNoFormProvider
+import play.api.data.Form
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import views.html.RegisterCharityWithARegulatorView
+import java.text.DecimalFormat
+
+class RegisterCharityWithARegulatorController @Inject() (
+  val controllerComponents: MessagesControllerComponents,
+  view: RegisterCharityWithARegulatorView,
+  actions: Actions,
+  formProvider: YesNoFormProvider,
+  appConfig: FrontendAppConfig
+) extends BaseController {
+
+  val form: Form[Boolean] = formProvider("registerCharityWithARegulator.error.required")
+
+  // TODO: After F9 is implemented this will be dynamic
+  // Currently hardcoded to display exceptedLimit (£100,000)
+  lazy val formattedLimit: String = new DecimalFormat("#,###").format(appConfig.exceptedLimit)
+
+  def onPageLoad: Action[AnyContent] = actions.authAndGetData() { implicit request =>
+    Ok(view(appConfig.registerCharityWithARegulatorUrl, formattedLimit)(form))
+  }
+
+  def onSubmit: Action[AnyContent] = actions.authAndGetData() { implicit request =>
+    form
+      .bindFromRequest()
+      .fold(
+        formWithErrors => BadRequest(view(appConfig.registerCharityWithARegulatorUrl, formattedLimit)(formWithErrors)),
+        {
+          case true =>
+            // TODO: User selected 'Yes' - redirect to placeholder R2 screen, route to be updated in the future
+            Redirect(controllers.organisationDetails.routes.MakeCharityRepaymentClaimController.onPageLoad)
+
+          case false =>
+            // TODO: User selected 'No' - redirect to D3 placeholder screen, route to be updated in the future
+            Redirect(controllers.routes.DeclarationDetailsConfirmationController.onPageLoad)
+        }
+      )
+  }
+}
