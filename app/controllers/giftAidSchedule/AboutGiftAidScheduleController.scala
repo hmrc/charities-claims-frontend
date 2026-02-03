a6470f26606bda3d132f054cdbf7db8513c8a4de
pvarsani <243989389+pvarsani@users.noreply.github.com>
2026-02-03 15:09:08 +0000

    About Gift Aid Schedule screen and test

---
+++ app/controllers/giftAidSchedule/AboutGiftAidScheduleController.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.giftAidSchedule
+
+import com.google.inject.Inject
+import controllers.actions.Actions
+import controllers.giftAidSchedule.routes
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.AboutGiftAidScheduleView
+
+import scala.concurrent.Future
+
+class AboutGiftAidScheduleController @Inject() (
+  val controllerComponents: MessagesControllerComponents,
+  actions: Actions,
+  view: AboutGiftAidScheduleView
+) extends FrontendBaseController
+    with I18nSupport {
+
+  val onPageLoad: Action[AnyContent] = actions.authAndGetData() { implicit request =>
+    Ok(view())
+  }
+
+  val onSubmit: Action[AnyContent] = actions.authAndGetData().async { implicit request =>
+    Future.successful(Redirect(routes.UploadGiftAidScheduleController.onPageLoad))
+  }
+}
---
+++ test/controllers/giftAidSchedule/AboutGiftAidScheduleControllerSpec.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.giftAidSchedule
+
+import controllers.ControllerSpec
+import controllers.giftAidSchedule.routes
+import play.api.Application
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.FakeRequest
+import views.html.AboutGiftAidScheduleView
+
+class AboutGiftAidScheduleControllerSpec extends ControllerSpec {
+  "AboutGiftAidScheduleController" - {
+    "onPageLoad" - {
+      "should render the page correctly" in {
+        given application: Application = applicationBuilder().build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(GET, routes.AboutGiftAidScheduleController.onPageLoad.url)
+
+          val result = route(application, request).value
+
+          val view = application.injector.instanceOf[AboutGiftAidScheduleView]
+
+          status(result) shouldEqual OK
+
+          contentAsString(result) shouldEqual view().body
+        }
+      }
+    }
+
+    "onSubmit" - {
+      "should redirect to the next page" in {
+        given application: Application = applicationBuilder().build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(POST, routes.AboutGiftAidScheduleController.onSubmit.url)
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(routes.UploadGiftAidScheduleController.onPageLoad.url)
+        }
+      }
+    }
+  }
+}
