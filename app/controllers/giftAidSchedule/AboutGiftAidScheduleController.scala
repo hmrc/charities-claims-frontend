43fd1462ee0b19222c2e2528db07abbad70759b1
Purbai <243989389+pvarsani@users.noreply.github.com>
2026-02-05 13:29:24 +0000

    DTR-2413 About Gift Aid Schedule screen and test (#188)
    * About Gift Aid Schedule screen and test
    
    * move the url to application conf and add dataguard
    
    * add unit test
    
    * unit test
    
    * fix broken unit tests
    
    * remove redundant import

---
+++ app/controllers/giftAidSchedule/AboutGiftAidScheduleController.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.giftAidSchedule
+
+import com.google.inject.Inject
+import config.FrontendAppConfig
+import controllers.BaseController
+import controllers.actions.Actions
+import controllers.giftAidSchedule.routes
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import views.html.AboutGiftAidScheduleView
+import models.RepaymentClaimDetailsAnswers
+
+import scala.concurrent.Future
+
+class AboutGiftAidScheduleController @Inject() (
+  val controllerComponents: MessagesControllerComponents,
+  actions: Actions,
+  view: AboutGiftAidScheduleView,
+  appConfig: FrontendAppConfig
+) extends BaseController {
+
+  def onPageLoad: Action[AnyContent] = actions.authAndGetData().async { implicit request =>
+    if RepaymentClaimDetailsAnswers.getClaimingGiftAid.contains(true) then {
+      Future.successful(Ok(view(appConfig.giftAidScheduleSpreadsheetsToClaimBackTaxOnDonationsUrl)))
+    } else {
+      Future.successful(Redirect(controllers.routes.PageNotFoundController.onPageLoad))
+    }
+  }
+
+  def onSubmit: Action[AnyContent] = actions.authAndGetData().async { implicit request =>
+    Future.successful(Redirect(routes.UploadGiftAidScheduleController.onPageLoad))
+  }
+}
---
+++ test/controllers/giftAidSchedule/AboutGiftAidScheduleControllerSpec.scala
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.giftAidSchedule
+
+import controllers.ControllerSpec
+import controllers.giftAidSchedule.routes
+import models.RepaymentClaimDetailsAnswers
+import play.api.Application
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.FakeRequest
+
+class AboutGiftAidScheduleControllerSpec extends ControllerSpec {
+  "AboutGiftAidScheduleController" - {
+    "onPageLoad" - {
+
+      "should render Page Not Found if setClaimingGiftAid is false" in {
+        val sessionData                = RepaymentClaimDetailsAnswers.setClaimingGiftAid(false)
+        val customConfig               = Map(
+          "urls.giftAidScheduleSpreadsheetsToClaimBackTaxOnDonationsUrl" -> "https://test.example.com/charity-repayment-claim"
+        )
+        given application: Application = applicationBuilder(sessionData = sessionData).configure(customConfig).build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(GET, routes.AboutGiftAidScheduleController.onPageLoad.url)
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(controllers.routes.PageNotFoundController.onPageLoad.url)
+        }
+      }
+
+      "should use the correct configured giftAidScheduleSpreadsheetsToClaimBackTaxOnDonationsUrl in the message" in {
+        val sessionData  = RepaymentClaimDetailsAnswers.setClaimingGiftAid(true)
+        val customConfig = Map(
+          "urls.giftAidScheduleSpreadsheetsToClaimBackTaxOnDonationsUrl" -> "https://test.example.com/charity-repayment-claim"
+        )
+
+        given application: Application = applicationBuilder(sessionData = sessionData)
+          .configure(customConfig)
+          .build()
+
+        running(application) {
+          val request =
+            FakeRequest(GET, routes.AboutGiftAidScheduleController.onPageLoad.url)
+          val result  = route(application, request).value
+
+          status(result) shouldEqual OK
+          contentAsString(result) should include("https://test.example.com/charity-repayment-claim")
+        }
+      }
+    }
+
+    "onSubmit" - {
+      "should redirect to the next page" in {
+        given application: Application = applicationBuilder().build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(POST, routes.AboutGiftAidScheduleController.onSubmit.url)
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(routes.UploadGiftAidScheduleController.onPageLoad.url)
+        }
+      }
+    }
+  }
+}
