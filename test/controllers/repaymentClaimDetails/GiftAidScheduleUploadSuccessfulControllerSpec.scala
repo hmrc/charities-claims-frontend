3617008f1c0cf789c85fa608d94ebb48200978af
nimo-mohamed <138133949+nimo-mohamed@users.noreply.github.com>
2026-02-06 13:42:37 +0000

    DTR-2914: CHARITIES: Gift Aid Schedule - Screen G1.5 - Upload successful

---
+++ test/controllers/repaymentClaimDetails/GiftAidScheduleUploadSuccessfulControllerSpec.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.repaymentClaimDetails
+
+import controllers.ControllerSpec
+import play.api.Application
+import play.api.test.FakeRequest
+import views.html.GiftAidScheduleUploadSuccessfulView
+import play.api.i18n.MessagesApi
+
+class GiftAidScheduleUploadSuccessfulControllerSpec extends ControllerSpec {
+
+  "GiftAidScheduleUploadSuccessfulController" - {
+
+    "onPageLoad" - {
+      "should render the page correctly" in {
+        given application: Application = applicationBuilder().build()
+
+        running(application) {
+          val request = FakeRequest(GET, routes.GiftAidScheduleUploadSuccessfulController.onPageLoad.url)
+          val result  = route(application, request).value
+          val view    = application.injector.instanceOf[GiftAidScheduleUploadSuccessfulView]
+
+          val messages = application.injector.instanceOf[MessagesApi].preferred(request)
+
+          status(result) shouldEqual OK
+          contentAsString(result) shouldEqual view()(request, messages).body
+        }
+      }
+    }
+
+    "onSubmit" - {
+      "should redirect to the Claims Task List" in {
+        given application: Application = applicationBuilder().build()
+
+        running(application) {
+          val request = FakeRequest(POST, routes.GiftAidScheduleUploadSuccessfulController.onSubmit.url)
+          val result  = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(controllers.routes.ClaimsTaskListController.onPageLoad.url)
+        }
+      }
+    }
+  }
+}
