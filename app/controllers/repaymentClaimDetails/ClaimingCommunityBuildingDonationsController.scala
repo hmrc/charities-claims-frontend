b24d9b602233270c39bda83cfeab7b8f1356dfae
Purbai <243989389+pvarsani@users.noreply.github.com>
2026-02-11 14:35:04 +0000

    DTR-2378: redirection when screen r1.2 is hopped (#214)
    * redirection when screen r1.2 is hopped
    
    * redirection when screen r1.2 is hopped for checkmode as well

--- app/controllers/repaymentClaimDetails/ClaimingCommunityBuildingDonationsController.scala
+++ app/controllers/repaymentClaimDetails/ClaimingCommunityBuildingDonationsController.scala
@@ -163,7 +163,8 @@ object ClaimingCommunityBuildingDonationsController {
 
       // NormalMode: User answered No
       case (false, NormalMode, _)          =>
-        if prevScreenAnswer.contains(false) then routes.ConnectedToAnyOtherCharitiesController.onPageLoad(NormalMode)
+        if prevScreenAnswer.contains(false) || prevScreenAnswer.isEmpty then
+          routes.ConnectedToAnyOtherCharitiesController.onPageLoad(NormalMode)
         else routes.ChangePreviousGASDSClaimController.onPageLoad(NormalMode)
 
       // CheckMode: New answer is Yes
@@ -172,7 +173,7 @@ object ClaimingCommunityBuildingDonationsController {
 
       // CheckMode: New answer is No
       case (false, CheckMode, None)        =>
-        if prevScreenAnswer.contains(false) then routes.ConnectedToAnyOtherCharitiesController.onPageLoad(CheckMode)
+        if prevScreenAnswer.contains(false) || prevScreenAnswer.isEmpty then routes.ConnectedToAnyOtherCharitiesController.onPageLoad(CheckMode)
         else routes.ChangePreviousGASDSClaimController.onPageLoad(CheckMode)
 
       // CheckMode: Answer unchanged yes
