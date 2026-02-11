9335877524bf46e0fb6eb04ddb37941f600e6eb3
Artur Opala <250927+arturopala@users.noreply.github.com>
2026-02-11 16:03:44 +0100

    [DTR-2914] CHARITIES: Gift Aid Schedule - Screen G1.5 - Upload successful (#213)

---
+++ test/controllers/giftAidSchedule/GiftAidScheduleUploadSuccessfulControllerSpec..scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.giftAidSchedule
+
+import controllers.ControllerSpec
+import controllers.giftAidSchedule.routes
+import models.RepaymentClaimDetailsAnswers
+import play.api.Application
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.FakeRequest
+import util.TestScheduleData
+import models.FileUploadReference
+import views.html.GiftAidScheduleUploadSuccessfulView
+import play.api.i18n.MessagesApi
+import play.api.i18n.Lang
+
+class GiftAidScheduleUploadSuccessfulControllerSpec extends ControllerSpec {
+
+  "GiftAidScheduleUploadSuccessfulControllerSpec" - {
+    "onPageLoad" - {
+
+      "should render page successfully" in {
+        val sessionData = RepaymentClaimDetailsAnswers
+          .setClaimingGiftAid(false)
+          .copy(
+            giftAidScheduleFileUploadReference = Some(FileUploadReference("test-file-upload-reference")),
+            giftAidScheduleData = Some(TestScheduleData.exampleGiftAidScheduleData)
+          )
+
+        val application = applicationBuilder(sessionData = sessionData).build()
+        val view        = application.injector.instanceOf[GiftAidScheduleUploadSuccessfulView]
+        val messages    = application.injector.instanceOf[MessagesApi].preferred(Seq(Lang("en")))
+
+        running(application) {
+          val request = FakeRequest(GET, routes.GiftAidScheduleUploadSuccessfulController.onPageLoad.url)
+
+          val result = route(application, request).value
+          status(result) shouldEqual OK
+          contentAsString(result) shouldBe view()(using request, messages).body
+        }
+      }
+
+    }
+
+    "onSubmit" - {
+      "should redirect to the next page" in {
+        val application: Application = applicationBuilder().build()
+
+        running(application) {
+          val request = FakeRequest(POST, routes.GiftAidScheduleUploadSuccessfulController.onSubmit.url)
+
+          val result = route(application, request).value
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(controllers.routes.ClaimsTaskListController.onPageLoad.url)
+        }
+      }
+    }
+  }
+}
