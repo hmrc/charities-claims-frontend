d3fa65426cf9b8f8847eaa531fd85102dbb40556
Artur Opala <250927+arturopala@users.noreply.github.com>
2026-02-17 15:39:59 +0000

    [DTR-3258] CHARITIES: Warning Screen WRN7.1 - Rejected - Gift Aid schedule  + messages sorted

---
+++ test/controllers/giftAidSchedule/ProblemUpdatingGiftAidScheduleRejectedControllerSpec.scala
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.giftAidSchedule
+
+import play.api.test.FakeRequest
+import play.api.mvc.AnyContentAsEmpty
+import controllers.ControllerSpec
+import views.html.ScheduleUploadFailureView
+import play.api.Application
+import models.RepaymentClaimDetailsAnswers
+
+class ProblemUpdatingGiftAidScheduleRejectedControllerSpec extends ControllerSpec {
+
+  "ProblemUpdatingGiftAidScheduleRejectedController" - {
+    "onPageLoad" - {
+      "should render the page correctly" in {
+        val sessionData                = RepaymentClaimDetailsAnswers.setClaimingGiftAid(true)
+        given application: Application = applicationBuilder(sessionData = sessionData)
+          .build()
+
+        running(application) {
+          val request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(GET, routes.ProblemUpdatingGiftAidScheduleRejectedController.onPageLoad.url)
+
+          val result = route(application, request).value
+          val view   = application.injector.instanceOf[ScheduleUploadFailureView]
+          val msgs   = application.injector.instanceOf[play.api.i18n.MessagesApi].preferred(request)
+
+          status(result) shouldBe OK
+
+          contentAsString(result) shouldBe view(
+            messagesKeyPrefix = "problemUpdatingGiftAidScheduleRejected",
+            submitAction = routes.ProblemUpdatingGiftAidScheduleRejectedController.onSubmit,
+            dashboardLink = controllers.routes.ClaimsTaskListController.onPageLoad
+          )(using request, msgs).body
+        }
+      }
+
+      "should refuse when schedule checkbox is not selected" in {
+        val sessionData = RepaymentClaimDetailsAnswers.setClaimingGiftAid(false)
+
+        val application: Application = applicationBuilder(sessionData = sessionData).build()
+
+        running(application) {
+          val request = FakeRequest(GET, routes.ProblemUpdatingGiftAidScheduleRejectedController.onPageLoad.url)
+          val result  = route(application, request).value
+          status(result)           shouldBe SEE_OTHER
+          redirectLocation(result) shouldBe Some(controllers.routes.PageNotFoundController.onPageLoad.url)
+        }
+      }
+    }
+
+    "onSubmit" - {
+      "should redirect to the next page" in {
+        val sessionData              = RepaymentClaimDetailsAnswers.setClaimingGiftAid(true)
+        val application: Application = applicationBuilder(sessionData = sessionData).build()
+
+        running(application) {
+          val request = FakeRequest(POST, routes.ProblemUpdatingGiftAidScheduleRejectedController.onSubmit.url)
+          val result  = route(application, request).value
+          status(result)           shouldBe SEE_OTHER
+          redirectLocation(result) shouldBe Some(routes.UploadGiftAidScheduleController.onPageLoad.url)
+        }
+      }
+
+      "should refuse when schedule checkbox is not selected" in {
+        val sessionData = RepaymentClaimDetailsAnswers.setClaimingGiftAid(false)
+
+        val application: Application = applicationBuilder(sessionData = sessionData).build()
+
+        running(application) {
+          val request = FakeRequest(POST, routes.ProblemUpdatingGiftAidScheduleRejectedController.onSubmit.url)
+          val result  = route(application, request).value
+          status(result)           shouldBe SEE_OTHER
+          redirectLocation(result) shouldBe Some(controllers.routes.PageNotFoundController.onPageLoad.url)
+        }
+      }
+    }
+  }
+}
