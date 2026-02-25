73f14be729de48e42abb1b7e18745c5e4c78816c
suryabalasingh <22298166+suryabalasingh@users.noreply.github.com>
2026-02-25 10:51:13 +0000

    DTR-3076 and DTR-3322, Implemented Upload Status and Rejected warning screen for connected charities

--- app/controllers/connectedCharitiesSchedule/YourConnectedCharitiesScheduleUploadController.scala
+++ app/controllers/connectedCharitiesSchedule/YourConnectedCharitiesScheduleUploadController.scala
@@ -17,19 +17,147 @@
 package controllers.connectedCharitiesSchedule
 
 import com.google.inject.Inject
+import connectors.ClaimsValidationConnector
 import controllers.BaseController
 import controllers.actions.Actions
-import models.SessionData
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import controllers.connectedCharitiesSchedule.routes
+import models.*
+import play.api.mvc.*
+import services.{ClaimsValidationService, SaveService}
+import views.html.YourConnectedCharitiesScheduleUploadView
+
+import scala.concurrent.{ExecutionContext, Future}
 
 class YourConnectedCharitiesScheduleUploadController @Inject() (
   val controllerComponents: MessagesControllerComponents,
-  actions: Actions
-) extends BaseController {
+  view: YourConnectedCharitiesScheduleUploadView,
+  actions: Actions,
+  claimsValidationConnector: ClaimsValidationConnector,
+  claimsValidationService: ClaimsValidationService,
+  saveService: SaveService
+)(using ec: ExecutionContext)
+    extends BaseController {
 
   def onPageLoad: Action[AnyContent] =
     actions
-      .authAndGetDataWithGuard(SessionData.shouldUploadConnectedCharitiesSchedule) { implicit request =>
-        Ok("Your Connected Charities schedule upload - placeholder")
+      .authAndGetDataWithGuard(SessionData.shouldUploadConnectedCharitiesSchedule)
+      .async { implicit request =>
+        val claimId = request.sessionData.unsubmittedClaimId.get
+
+        claimsValidationService
+          .getFileUploadReference(ValidationType.ConnectedCharities, acceptAwaitingUpload = false)
+          .flatMap {
+            case None =>
+              // if the file upload reference is not found, we need to redirect to the upload page to start a new upload
+              Future.successful(Redirect(routes.UploadConnectedCharitiesScheduleController.onPageLoad))
+
+            case Some(fileUploadReference) =>
+              claimsValidationConnector
+                .getUploadResult(claimId, fileUploadReference)
+                .map {
+                  case uploadResult: GetUploadResultVeryfying =>
+                    Ok(
+                      view(
+                        claimId = claimId,
+                        uploadResult = uploadResult,
+                        failureDetails = None,
+                        screenLocked = true
+                      )
+                    )
+
+                  case uploadResult: GetUploadResultValidating =>
+                    Ok(
+                      view(
+                        claimId = claimId,
+                        uploadResult = uploadResult,
+                        failureDetails = None,
+                        screenLocked = true
+                      )
+                    )
+
+                  case uploadResult @ GetUploadResultVeryficationFailed(
+                        reference,
+                        validationType,
+                        failureDetails
+                      ) =>
+                    Ok(
+                      view(
+                        claimId = claimId,
+                        uploadResult = uploadResult,
+                        failureDetails = Some(failureDetails),
+                        screenLocked = false
+                      )
+                    )
+
+                  case uploadResult =>
+                    Ok(
+                      view(
+                        claimId = claimId,
+                        uploadResult = uploadResult,
+                        failureDetails = None,
+                        screenLocked = false
+                      )
+                    )
+                }
+                .recoverWith {
+                  case e: Exception if e.getMessage.contains("CLAIM_REFERENCE_DOES_NOT_EXIST") =>
+                    saveService
+                      .save(request.sessionData.copy(connectedCharitiesScheduleFileUploadReference = None))
+                      .map(_ => Redirect(routes.UploadConnectedCharitiesScheduleController.onPageLoad))
+                }
+          }
       }
+
+  def onRemove: Action[AnyContent] =
+    actions
+      .authAndGetDataWithGuard(SessionData.shouldUploadConnectedCharitiesSchedule)
+      .async { implicit request =>
+        for {
+          _ <- claimsValidationService.deleteConnectedCharitiesSchedule
+        } yield Redirect(routes.UploadConnectedCharitiesScheduleController.onPageLoad)
+      }
+
+  def onSubmit: Action[AnyContent] =
+    actions
+      .authAndGetDataWithGuard(SessionData.shouldUploadConnectedCharitiesSchedule)
+      .async { implicit request =>
+        val claimId = request.sessionData.unsubmittedClaimId.get
+
+        request.sessionData.connectedCharitiesScheduleFileUploadReference match {
+          case None =>
+            // if the file upload reference is not found, we need to redirect to the upload page to start a new upload
+            Future.successful(Redirect(routes.UploadConnectedCharitiesScheduleController.onPageLoad))
+
+          case Some(fileUploadReference) =>
+            claimsValidationConnector
+              .getUploadResult(claimId, fileUploadReference)
+              .flatMap {
+                case failure: GetUploadResultVeryficationFailed =>
+                  claimsValidationService.deleteConnectedCharitiesSchedule
+                    .map(_ =>
+                      Redirect {
+                        failure.failureDetails.failureReason match {
+                          case FailureReason.QUARANTINE =>
+                            routes.UploadConnectedCharitiesScheduleController.onPageLoad
+                          case FailureReason.REJECTED   =>
+                            routes.ProblemUpdatingConnectedCharitiesScheduleRejectedController.onPageLoad
+                          case _                        =>
+                            routes.UploadConnectedCharitiesScheduleController.onPageLoad
+                        }
+                      }
+                    )
+
+                case _: GetUploadResultValidatedConnectedCharities =>
+                  Future.successful(Redirect(routes.CheckYourConnectedCharitiesScheduleController.onPageLoad))
+
+                case _: GetUploadResultValidationFailedConnectedCharities =>
+                  Future.successful(Redirect(routes.ProblemWithConnectedCharitiesScheduleController.onPageLoad))
+
+                case _ =>
+                  // strange case, but we need to handle it
+                  Future.successful(Redirect(routes.YourConnectedCharitiesScheduleUploadController.onPageLoad))
+              }
+        }
+      }
+
 }
