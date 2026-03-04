95d25367104080e15e12d01e8f712bce44a13591
suryabalasingh <22298166+suryabalasingh@users.noreply.github.com>
2026-03-04 09:13:34 +0000

    Implemented Refresh upload for connected charities

--- test/controllers/connectedCharitiesSchedule/YourConnectedCharitiesScheduleUploadControllerSpec.scala
+++ test/controllers/connectedCharitiesSchedule/YourConnectedCharitiesScheduleUploadControllerSpec.scala
@@ -161,7 +161,10 @@ class YourConnectedCharitiesScheduleUploadControllerSpec extends ControllerSpec 
             claimId,
             testAwaitingResponse,
             None,
-            false
+            false,
+            routes.ConnectedCharitiesScheduleUploadStatusController.status,
+            3,
+            routes.YourConnectedCharitiesScheduleUploadController.onPageLoad
           ).body
 
         }
@@ -191,7 +194,10 @@ class YourConnectedCharitiesScheduleUploadControllerSpec extends ControllerSpec 
             claimId,
             testVerifyingResponse,
             None,
-            true
+            true,
+            routes.ConnectedCharitiesScheduleUploadStatusController.status,
+            3,
+            routes.YourConnectedCharitiesScheduleUploadController.onPageLoad
           ).body
         }
       }
@@ -220,7 +226,10 @@ class YourConnectedCharitiesScheduleUploadControllerSpec extends ControllerSpec 
             claimId,
             testValidatingResponse,
             None,
-            true
+            true,
+            routes.ConnectedCharitiesScheduleUploadStatusController.status,
+            3,
+            routes.YourConnectedCharitiesScheduleUploadController.onPageLoad
           ).body
         }
       }
@@ -248,7 +257,10 @@ class YourConnectedCharitiesScheduleUploadControllerSpec extends ControllerSpec 
             claimId,
             testVerificationFailedResponse,
             None,
-            false
+            false,
+            routes.ConnectedCharitiesScheduleUploadStatusController.status,
+            3,
+            routes.YourConnectedCharitiesScheduleUploadController.onPageLoad
           ).body
         }
       }
