66b7bf39eb5af71b02931e2465f26d5d2782b0c1
Purbai <243989389+pvarsani@users.noreply.github.com>
2026-04-07 09:29:12 +0100

    DTR-3817 : screen S2.1 - about GASDS screen (#351)
    * screen S2.1
    
    * screen S2.1

---
+++ test/controllers/giftAidSmallDonationsScheme/AboutGiftAidSmallDonationsSchemeControllerSpec.scala
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.giftAidSmallDonationsScheme
+
+import controllers.ControllerSpec
+import play.api.Application
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.FakeRequest
+import views.html.AboutGiftAidSmallDonationsSchemeView
+
+class AboutGiftAidSmallDonationsSchemeControllerSpec extends ControllerSpec {
+  "AboutGiftAidSmallDonationsSchemeController" - {
+    "onPageLoad" - {
+      "should render the page correctly" in {
+        val sessionData = completeGasdsSession
+
+        given application: Application = applicationBuilder(sessionData = sessionData).build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(GET, routes.AboutGiftAidSmallDonationsSchemeController.onPageLoad.url)
+
+          val result = route(application, request).value
+
+          val view = application.injector.instanceOf[AboutGiftAidSmallDonationsSchemeView]
+
+          status(result) shouldEqual OK
+
+          contentAsString(result) shouldEqual view().body
+        }
+      }
+
+      "should render ClaimsTaskListController if completeGasdsSession is false" in {
+        given application: Application = applicationBuilder().build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(GET, routes.AboutGiftAidSmallDonationsSchemeController.onPageLoad.url)
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(controllers.routes.ClaimsTaskListController.onPageLoad.url)
+        }
+      }
+    }
+
+    "onSubmit" - {
+      "should redirect to the next page" in {
+        val sessionData = completeGasdsSession
+
+        given application: Application = applicationBuilder(sessionData = sessionData).build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(POST, routes.AboutGiftAidSmallDonationsSchemeController.onSubmit.url)
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          // TODO - change to the next page when availale
+          redirectLocation(result) shouldEqual Some(controllers.routes.ClaimsTaskListController.onPageLoad.url)
+        }
+      }
+
+      "should redirect to the ClaimsTaskListController when completeGasdsSession is false" in {
+        given application: Application = applicationBuilder().build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(POST, routes.AboutGiftAidSmallDonationsSchemeController.onSubmit.url)
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(
+            controllers.routes.ClaimsTaskListController.onPageLoad.url
+          )
+        }
+      }
+    }
+  }
+
+}
