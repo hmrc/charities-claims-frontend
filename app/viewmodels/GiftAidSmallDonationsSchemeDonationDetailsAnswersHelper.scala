b7ae84ecc258bba36844b16d9da49a9cf939b441
Sandip Khedekar <17478517+SandipKhedekar@users.noreply.github.com>
2026-04-29 13:35:56 +0100

    [DTR-4142] - Navigation to WRN6 page (#420)

--- app/viewmodels/GiftAidSmallDonationsSchemeDonationDetailsAnswersHelper.scala
+++ app/viewmodels/GiftAidSmallDonationsSchemeDonationDetailsAnswersHelper.scala
@@ -22,7 +22,7 @@ import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.{ActionItem, Actio
 import play.api.i18n.Messages
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.*
 import models.GiftAidSmallDonationsSchemeDonationDetailsAnswers
-import models.Mode.{CheckMode, NormalMode}
+import models.Mode.*
 
 import scala.math.BigDecimal.RoundingMode
 
@@ -65,9 +65,6 @@ object GiftAidSmallDonationsSchemeDonationDetailsAnswersHelper {
     val label =
       messages("giftAidSmallDonationsSchemeDetailsCheckYourAnswers.numberOfTaxYearsAdded.label")
 
-    val changeOrEnterUrl =
-      controllers.giftAidSmallDonationsScheme.routes.ClaimAddedForTaxYearController.onPageLoad.url
-
     val taxYearsCount: Int = answers
       .flatMap(_.claims)
       .fold(0)(_.flatten.size)
@@ -78,14 +75,16 @@ object GiftAidSmallDonationsSchemeDonationDetailsAnswersHelper {
         .fold(
           missingDataRow(
             label,
-            changeOrEnterUrl,
+            controllers.giftAidSmallDonationsScheme.routes.WhichTaxYearAreYouClaimingForController
+              .onPageLoad(1, NormalMode)
+              .url,
             label
           )
         )(count =>
           summaryRow(
             label,
             count.toString,
-            changeOrEnterUrl,
+            controllers.giftAidSmallDonationsScheme.routes.ClaimAddedForTaxYearController.onPageLoad.url,
             label
           )
         )
