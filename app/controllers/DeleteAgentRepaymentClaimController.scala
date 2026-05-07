26427ae841146a1a51332950b85336211defc41b
Artur Opala <250927+arturopala@users.noreply.github.com>
2026-05-07 16:57:20 +0100

    [DTR-4143] CHARITIES: [AGENT FLOW] Screen WRN10 - Delete draft repayment claim

---
+++ app/controllers/DeleteAgentRepaymentClaimController.scala
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import services.SaveService
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import com.google.inject.Inject
+import com.google.inject.Singleton
+import connectors.ClaimsConnector
+import controllers.BaseController
+import config.FrontendAppConfig
+import views.html.DeleteAgentRepaymentClaimView
+import controllers.actions.{AccessType, Actions, GuardAction}
+import play.api.Logging
+import forms.YesNoFormProvider
+import models.{RepaymentClaimDetailsAnswers, SessionData}
+import play.api.data.Form
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class DeleteAgentRepaymentClaimController @Inject() (
+  val controllerComponents: MessagesControllerComponents,
+  view: DeleteAgentRepaymentClaimView,
+  actions: Actions,
+  guard: GuardAction,
+  formProvider: YesNoFormProvider,
+  claimsConnector: ClaimsConnector,
+  saveService: SaveService,
+  appConfig: FrontendAppConfig
+)(using ec: ExecutionContext)
+    extends BaseController
+    with Logging {
+
+  val form: Form[Boolean] = formProvider("deleteAgentRepaymentClaim.error.required")
+
+  val onPageLoad: Action[AnyContent] =
+    actions
+      .authAndGetData()
+      .andThen(
+        guard(
+          predicate = SessionData.isClaimNotSubmitted,
+          access = AccessType.AgentOnly
+        )
+      )
+      .async { implicit request =>
+        request.sessionData.unsubmittedClaimId match {
+          case Some(claimId) =>
+            val charityName =
+              RepaymentClaimDetailsAnswers
+                .getNameOfCharity(using request.sessionData)
+                .getOrElse("")
+
+            Future.successful(Ok(view(form, claimId, charityName)))
+
+          case None =>
+            Future.successful(Redirect(appConfig.charityRepaymentDashboardUrl))
+        }
+      }
+
+  val onSubmit: Action[AnyContent] =
+    actions
+      .authAndGetData()
+      .andThen(
+        guard(
+          predicate = SessionData.isClaimNotSubmitted,
+          access = AccessType.AgentOnly
+        )
+      )
+      .async { implicit request =>
+        val formData = form.bindFromRequest()
+        formData.fold(
+          formWithErrors =>
+            request.sessionData.unsubmittedClaimId match {
+              case Some(claimId) =>
+                val charityName =
+                  RepaymentClaimDetailsAnswers
+                    .getNameOfCharity(using request.sessionData)
+                    .getOrElse("")
+
+                Future.successful(BadRequest(view(formWithErrors, claimId, charityName)))
+
+              case None =>
+                Future.successful(Redirect(appConfig.charityRepaymentDashboardUrl))
+            },
+          {
+            case false =>
+              Future.successful(Redirect(appConfig.charityRepaymentDashboardUrl))
+
+            case true =>
+              formData.data.get("claimId") match {
+                case Some(claimId) =>
+                  claimsConnector.deleteClaim(claimId).flatMap {
+                    case true =>
+                      saveService
+                        .save(SessionData(charitiesReference = request.sessionData.charitiesReference))
+                        .map(_ => Redirect(appConfig.charityRepaymentDashboardUrl))
+
+                    case false =>
+                      Future.failed(new RuntimeException("Failed to delete claim"))
+                  }
+
+                case None =>
+                  Future.failed(
+                    new RuntimeException(
+                      "No claimId found in form data when attempting to delete agent repayment claim"
+                    )
+                  )
+              }
+          }
+        )
+      }
+}
---
+++ test/controllers/DeleteAgentRepaymentClaimControllerSpec.scala
@@ -0,0 +1,243 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import play.api.test.FakeRequest
+import play.api.mvc.AnyContentAsEmpty
+import controllers.ControllerSpec
+import config.FrontendAppConfig
+import views.html.DeleteAgentRepaymentClaimView
+import play.api.Application
+import uk.gov.hmrc.auth.core.AffinityGroup
+import models.SessionData
+import models.RepaymentClaimDetailsAnswers
+import forms.YesNoFormProvider
+import play.api.mvc.AnyContentAsFormUrlEncoded
+import connectors.ClaimsConnector
+import services.SaveService
+import play.api.inject.bind
+import uk.gov.hmrc.http.HeaderCarrier
+import scala.concurrent.Future
+
+class DeleteAgentRepaymentClaimControllerSpec extends ControllerSpec {
+
+  "DeleteAgentRepaymentClaimController" - {
+    "onPageLoad" - {
+      "should render the page correctly for an agent" in {
+        given application: Application = applicationBuilder(
+          sessionData = SessionData(
+            charitiesReference = testCharitiesReference,
+            unsubmittedClaimId = Some("test-claim-123"),
+            repaymentClaimDetailsAnswers = Some(
+              RepaymentClaimDetailsAnswers(
+                nameOfCharity = Some("Test Charity ABC")
+              )
+            )
+          ),
+          affinityGroup = AffinityGroup.Agent
+        ).build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(GET, routes.DeleteAgentRepaymentClaimController.onPageLoad.url + "?claimId=test-claim-123")
+
+          val result = route(application, request).value
+          val view   = application.injector.instanceOf[DeleteAgentRepaymentClaimView]
+          val form   = application.injector.instanceOf[YesNoFormProvider]
+
+          status(result) shouldEqual OK
+          contentAsString(result) shouldEqual view(form(), "test-claim-123", "Test Charity ABC").body
+        }
+      }
+
+      "should reject requests for an organisation" in {
+        given application: Application = applicationBuilder(affinityGroup = AffinityGroup.Organisation).build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(GET, routes.DeleteAgentRepaymentClaimController.onPageLoad.url + "?claimId=test-claim-123")
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(controllers.routes.ClaimsTaskListController.onPageLoad.url)
+        }
+      }
+
+      "should redirect to the charity repayment dashboard when unsubmittedClaimId is not present" in {
+        given application: Application = applicationBuilder(affinityGroup = AffinityGroup.Agent).build()
+        val appConfig                  = application.injector.instanceOf[FrontendAppConfig]
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(GET, routes.DeleteAgentRepaymentClaimController.onPageLoad.url)
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(appConfig.charityRepaymentDashboardUrl)
+        }
+      }
+
+      "should redirect to the claim complete page when the claim is submitted" in {
+        given application: Application = applicationBuilder(
+          sessionData = SessionData(
+            charitiesReference = testCharitiesReference,
+            unsubmittedClaimId = Some("test-claim-123"),
+            submissionReference = Some("test-submission-123")
+          ),
+          affinityGroup = AffinityGroup.Agent
+        ).build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsEmpty.type] =
+            FakeRequest(GET, routes.DeleteAgentRepaymentClaimController.onPageLoad.url)
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(
+            controllers.claimDeclaration.routes.ClaimCompleteController.onPageLoad.url
+          )
+        }
+      }
+    }
+
+    "onSubmit" - {
+      "should delete the claim and redirect to the charity repayment dashboard when the user confirms" in {
+        val mockClaimsConnector: ClaimsConnector = mock[ClaimsConnector]
+        val mockSaveService: SaveService         = mock[SaveService]
+
+        given application: Application = applicationBuilder(
+          sessionData = SessionData(
+            charitiesReference = testCharitiesReference,
+            unsubmittedClaimId = Some("test-claim-123"),
+            repaymentClaimDetailsAnswers = Some(
+              RepaymentClaimDetailsAnswers(
+                nameOfCharity = Some("Test Charity ABC")
+              )
+            )
+          ),
+          affinityGroup = AffinityGroup.Agent
+        ).overrides(
+          bind[ClaimsConnector].toInstance(mockClaimsConnector),
+          bind[SaveService].toInstance(mockSaveService)
+        ).build()
+
+        (mockClaimsConnector
+          .deleteClaim(_: String)(using _: HeaderCarrier))
+          .expects("test-claim-123", *)
+          .returning(Future.successful(true))
+
+        (mockSaveService
+          .save(_: SessionData)(using _: HeaderCarrier))
+          .expects(*, *)
+          .returning(Future.successful(()))
+
+        val appConfig = application.injector.instanceOf[FrontendAppConfig]
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsFormUrlEncoded] =
+            FakeRequest(POST, routes.DeleteAgentRepaymentClaimController.onSubmit.url)
+              .withFormUrlEncodedBody("value" -> "true", "claimId" -> "test-claim-123")
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(appConfig.charityRepaymentDashboardUrl)
+        }
+      }
+
+      "should redirect to the charity repayment dashboard when claim deletion is not confirmed" in {
+        given application: Application = applicationBuilder(
+          sessionData = SessionData(
+            charitiesReference = testCharitiesReference,
+            unsubmittedClaimId = Some("test-claim-123"),
+            repaymentClaimDetailsAnswers = Some(
+              RepaymentClaimDetailsAnswers(
+                nameOfCharity = Some("Test Charity ABC")
+              )
+            )
+          ),
+          affinityGroup = AffinityGroup.Agent
+        ).build()
+
+        val appConfig = application.injector.instanceOf[FrontendAppConfig]
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsFormUrlEncoded] =
+            FakeRequest(POST, routes.DeleteAgentRepaymentClaimController.onSubmit.url)
+              .withFormUrlEncodedBody("value" -> "false", "claimId" -> "test-claim-123")
+
+          val result = route(application, request).value
+
+          status(result) shouldEqual SEE_OTHER
+          redirectLocation(result) shouldEqual Some(appConfig.charityRepaymentDashboardUrl)
+        }
+      }
+
+      "should reload the page with errors when a required field `value` is missing" in {
+        given application: Application = applicationBuilder(
+          sessionData = SessionData(
+            charitiesReference = testCharitiesReference,
+            unsubmittedClaimId = Some("test-claim-123"),
+            repaymentClaimDetailsAnswers = Some(
+              RepaymentClaimDetailsAnswers(
+                nameOfCharity = Some("Test Charity ABC")
+              )
+            )
+          ),
+          affinityGroup = AffinityGroup.Agent
+        ).build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsFormUrlEncoded] =
+            FakeRequest(POST, routes.DeleteAgentRepaymentClaimController.onSubmit.url)
+              .withFormUrlEncodedBody("claimId" -> "test-claim-123")
+
+          val result = route(application, request).value
+          status(result) shouldEqual BAD_REQUEST
+        }
+      }
+
+      "should throw an exception when a required field `claimId` is missing" in {
+        given application: Application = applicationBuilder(
+          sessionData = SessionData(
+            charitiesReference = testCharitiesReference,
+            unsubmittedClaimId = Some("test-claim-123"),
+            repaymentClaimDetailsAnswers = Some(
+              RepaymentClaimDetailsAnswers(
+                nameOfCharity = Some("Test Charity ABC")
+              )
+            )
+          ),
+          affinityGroup = AffinityGroup.Agent
+        ).build()
+
+        running(application) {
+          given request: FakeRequest[AnyContentAsFormUrlEncoded] =
+            FakeRequest(POST, routes.DeleteAgentRepaymentClaimController.onSubmit.url)
+              .withFormUrlEncodedBody("value" -> "true")
+
+          val result = route(application, request).value
+          a[RuntimeException] should be thrownBy result.futureValue
+        }
+      }
+    }
+  }
+
+}
