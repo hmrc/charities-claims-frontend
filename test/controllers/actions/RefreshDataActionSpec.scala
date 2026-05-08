a0511ae8d4a9ef0a5b2393a0b6a4ea453fb02243
Artur Opala <250927+arturopala@users.noreply.github.com>
2026-05-08 08:37:59 +0100

    [DTR-4136] Add support for claimId=blank query parameter in RefreshDataAction for organisation user (#470)

--- test/controllers/actions/RefreshDataActionSpec.scala
+++ test/controllers/actions/RefreshDataActionSpec.scala
@@ -223,4 +223,141 @@ class RefreshDataActionSpec extends BaseSpec {
     }
 
   }
+
+  "refines AuthorisedRequest into a DataRequest when submitted session data object exists and claimId=blank is provided" in {
+    val mockSessionCache              = mock[SessionCache]
+    val mockClaimsConnector           = mock[ClaimsConnector]
+    val mockClaimsValidationConnector = mock[ClaimsValidationConnector]
+    val action                        = new DefaultRefreshDataAction(
+      mockSessionCache,
+      mockClaimsConnector,
+      mockClaimsValidationConnector,
+      new DefaultDataRetrievalAction(
+        mockSessionCache,
+        mockClaimsConnector,
+        mockClaimsValidationConnector,
+        testFrontendAppConfig
+      ),
+      testFrontendAppConfig
+    )
+
+    val sessionData = SessionData
+      .empty(testCharitiesReference)
+      .copy(submissionReference = Some("submission-123"))
+
+    (mockSessionCache
+      .get()(using _: HeaderCarrier))
+      .expects(*)
+      .returning(Future.successful(Some(sessionData)))
+
+    (mockSessionCache
+      .store(_: SessionData)(using _: HeaderCarrier))
+      .expects(*, *)
+      .returning(Future.successful(()))
+
+    val request =
+      AuthorisedRequest(FakeRequest("GET", "/test?claimId=blank"), AffinityGroup.Organisation, testCharitiesReference)
+
+    val result = action.invokeBlock(
+      request,
+      (req: DataRequest[?]) =>
+        req.sessionData shouldBe SessionData.empty(testCharitiesReference)
+        Future.successful(Ok)
+    )
+    status(result) shouldBe OK
+  }
+
+  "refines AuthorisedRequest into a DataRequest when unsubmitted claim exists and claimId=blank is provided" in {
+    val mockSessionCache              = mock[SessionCache]
+    val mockClaimsConnector           = mock[ClaimsConnector]
+    val mockClaimsValidationConnector = mock[ClaimsValidationConnector]
+    val action                        = new DefaultRefreshDataAction(
+      mockSessionCache,
+      mockClaimsConnector,
+      mockClaimsValidationConnector,
+      new DefaultDataRetrievalAction(
+        mockSessionCache,
+        mockClaimsConnector,
+        mockClaimsValidationConnector,
+        testFrontendAppConfig
+      ),
+      testFrontendAppConfig
+    )
+
+    val sessionData =
+      RepaymentClaimDetailsAnswers
+        .setClaimingTaxDeducted(true)
+        .and(SessionData.setUnsubmittedClaimId("claim-123"))
+
+    (mockSessionCache
+      .get()(using _: HeaderCarrier))
+      .expects(*)
+      .returning(Future.successful(Some(sessionData)))
+
+    (mockSessionCache
+      .store(_: SessionData)(using _: HeaderCarrier))
+      .expects(*, *)
+      .returning(Future.successful(()))
+
+    val getUploadSummaryResponseWithGiftAidAwaitingUpload =
+      GetUploadSummaryResponse(uploads =
+        Seq(
+          UploadSummary(
+            reference = FileUploadReference("gift-aid-ref-123"),
+            validationType = ValidationType.GiftAid,
+            fileStatus = FileStatus.AWAITING_UPLOAD,
+            uploadUrl = Some("https://www.foo.bar.com"),
+            fields = Some(Map("key" -> "value"))
+          )
+        )
+      )
+
+    (mockClaimsValidationConnector
+      .getUploadSummary(_: String)(using _: HeaderCarrier))
+      .expects(*, *)
+      .returning(
+        Future.successful(getUploadSummaryResponseWithGiftAidAwaitingUpload)
+      )
+
+    val claim = TestClaims.testClaimWithRepaymentClaimDetailsOnly()
+
+    (mockClaimsConnector
+      .getClaim(_: String)(using _: HeaderCarrier))
+      .expects(*, *)
+      .returning(Future.successful(Some(claim)))
+
+    val request =
+      AuthorisedRequest(FakeRequest("GET", "/test?claimId=blank"), AffinityGroup.Organisation, testCharitiesReference)
+
+    val result = action.invokeBlock(
+      request,
+      (req: DataRequest[?]) =>
+        req.sessionData shouldBe
+          SessionData.from(claim, testCharitiesReference, Some(getUploadSummaryResponseWithGiftAidAwaitingUpload))
+
+        req.sessionData.organisationDetailsAnswers                        shouldBe None
+        req.sessionData.giftAidScheduleData                               shouldBe None
+        req.sessionData.giftAidScheduleFileUploadReference                shouldBe Some(FileUploadReference("gift-aid-ref-123"))
+        req.sessionData.giftAidScheduleUpscanInitialization               shouldBe Some(
+          UpscanInitiateResponse(
+            reference = UpscanReference("gift-aid-ref-123"),
+            uploadRequest = UploadRequest(
+              href = "https://www.foo.bar.com",
+              fields = Map("key" -> "value")
+            )
+          )
+        )
+        req.sessionData.includedAnyAdjustmentsInClaimPrompt               shouldBe None
+        req.sessionData.understandFalseStatements                         shouldBe None
+        req.sessionData.otherIncomeScheduleData                           shouldBe None
+        req.sessionData.otherIncomeScheduleFileUploadReference            shouldBe None
+        req.sessionData.communityBuildingsScheduleData                    shouldBe None
+        req.sessionData.communityBuildingsScheduleFileUploadReference     shouldBe None
+        req.sessionData.connectedCharitiesScheduleData                    shouldBe None
+        req.sessionData.connectedCharitiesScheduleFileUploadReference     shouldBe None
+        req.sessionData.giftAidSmallDonationsSchemeDonationDetailsAnswers shouldBe None
+        Future.successful(Ok)
+    )
+    status(result) shouldBe OK
+  }
 }
