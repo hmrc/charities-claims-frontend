005a2c86779213f1d9a0296c6bdd42249a0a6a8d
jowensoc <114502866+jowensoc@users.noreply.github.com>
2026-05-11 16:11:44 +0100

    updated screen and added tests for organisation and agent content

--- app/controllers/communityBuildingsSchedule/CommunityBuildingsScheduleUploadSuccessfulController.scala
+++ app/controllers/communityBuildingsSchedule/CommunityBuildingsScheduleUploadSuccessfulController.scala
@@ -33,7 +33,7 @@ class CommunityBuildingsScheduleUploadSuccessfulController @Inject() (
 
   val onPageLoad: Action[AnyContent] =
     actions.authAndGetDataWithGuard(SessionData.shouldUploadCommunityBuildingsSchedule).async { implicit request =>
-      if request.sessionData.communityBuildingsScheduleCompleted then Future.successful(Ok(view()))
+      if request.sessionData.communityBuildingsScheduleCompleted then Future.successful(Ok(view(request.isAgent)))
       else Future.successful(Redirect(controllers.routes.ClaimsTaskListController.onPageLoad))
     }
 
