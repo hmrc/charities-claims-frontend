6ccc7fd450b66768860f3e924858aa37cfa69a35
Artur Opala <250927+arturopala@users.noreply.github.com>
2026-07-22 12:25:48 +0100

    [DTR-7204] Handle UNSUBMITTED_CLAIM_EXISTS_FOR_CHARITY error when saving a claim

--- app/connectors/ClaimsConnector.scala
+++ app/connectors/ClaimsConnector.scala
@@ -192,10 +192,11 @@ class ClaimsConnectorImpl @Inject() (
   ): Future[O] = {
     logger.info(s"$method $url [requestId=${hc.requestId.map(_.value).getOrElse("-")}]")
     retryFor(s"$method $url") {
-      case _: UpdatedByAnotherUserException           => false
-      case _: UnsubmittedClaimsLimitExceededException => false
-      case _: UnknownClaimError                       => false
-      case _                                          => true
+      case _: UpdatedByAnotherUserException             => false
+      case _: UnsubmittedClaimsLimitExceededException   => false
+      case _: UnsubmittedClaimExistsForCharityException => false
+      case _: UnknownClaimError                         => false
+      case _                                            => true
     } {
       val request: RequestBuilder = method match {
         case "GET"    => http.get(URL(url))
